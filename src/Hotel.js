f9bf4709e749320b53652ddb2b5237e5febb0845
Olga Morgan <olga.morgan17@gmail.com>
2020-11-05 21:17:32 -0700

    Add hotel parent class and tests

---
+++ src/Hotel.js
@@ -0,0 +1,46 @@
+class Hotel {
+  constructor(bookingRepo = [], roomRepo = [], customerRepo = []) {
+    this.bookingRepo = bookingRepo,
+    this.roomRepo = roomRepo,
+    this.customerRepo = customerRepo
+  }
+
+  getInformationByValue(value, data, property) {
+    return data.filter(dataPiece => {
+      return property === null ? value === dataPiece : value === dataPiece[property];
+  })
+}
+
+  determineUpcomingBookings(booking) {
+    let todayDate = new Date().getTime();
+    let bookingDate = new Date(booking.bookingDate).getTime();
+    if(bookingDate > todayDate){
+      return 'Upcoming';
+    }
+    return 'Completed';
+  }
+
+  getTodayDate() {
+    let todayDate = new Date();
+    let date = ("0" + todayDate.getDate()).slice(-2);
+    let month = ("0" + (todayDate.getMonth() + 1)).slice(-2);
+    let year = todayDate.getFullYear();
+    return `${year}/${month}/${date}`;
+  }
+
+  filterBookingByDate(bookings) {
+    return bookings.sort((a, b) => {
+      return a.bookingDate > b.bookingDate ? -1 : 1;
+    })
+  }
+
+  getAvailableRooms(bookingRepo, roomRepo, date) {
+    let bookingByDate = this.getInformationByValue(date, bookingRepo, 'bookingDate');
+    let availableRooms = roomRepo
+      .filter(room => bookingByDate
+      .some(booking => room.number !== booking.roomNumber));
+  	return availableRooms;
+  }
+}
+
+export default Hotel;
---
+++ test/hotel-test.js
@@ -0,0 +1,81 @@
+import { expect } from 'chai';
+
+import Booking from '../src/Booking'
+import BookingRepo from '../src/BookingRepo'
+import Room from '../src/Room'
+import RoomRepo from '../src/RoomRepo'
+import Customer from '../src/Customer'
+import CustomerRepo from '../src/CustomerRepo'
+import Manager from '../src/Manager';
+import Hotel from '../src/Hotel';
+
+describe('Hotel', function() {
+  let booking1,
+      booking2,
+      booking3,
+      bookings,
+      bookingRepo,
+      room1,
+      room2,
+      room3,
+      room4,
+      room5,
+      rooms,
+      roomRepo,
+      customer1,
+      customer2,
+      customers,
+      customerRepo,
+      manager,
+      hotel;
+
+  beforeEach(function() {
+    customer1 = new Customer(1, 'Lisa Kawalski');
+    customer2 = new Customer(2, 'Mike Kawalski');
+    customers = [customer1, customer2];
+    customerRepo = new CustomerRepo(customers);
+
+    room1 = new Room(1, "residential suite", false, "queen", 3, 358.4);
+    room2 = new Room(2, "suite", true, "twin", 2, 400.34);
+    room3 = new Room(3, "single room", false, "twin", 1, 300.2);
+    room4 = new Room(4, "suite", true, "queen", 1, 650.90);
+    rooms = [room1, room2, room3, room4];
+    roomRepo = new RoomRepo(rooms);
+
+    booking1 = new Booking("5fwrgu4i7k55hl7q8", 43, "2020/01/24", 3);
+    booking2 = new Booking("5fwrgu4i7k55hl6t5", 13, "2020/02/10", 1);
+    bookings = [booking1, booking2];
+    bookingRepo = new BookingRepo(bookings);
+
+    manager = new Manager(bookingRepo, roomRepo, customerRepo)
+    hotel = new Hotel(bookingRepo.bookingRepo, roomRepo.roomRepo, customerRepo.customerRepo)
+  });
+
+  it('should be a function', () => {
+    expect(Hotel).to.be.a('function');
+  });
+
+  it('should be an instance of a Manager class', () => {
+    expect(hotel).to.be.a.instanceOf(Hotel);
+  });
+
+  it('should get available Rooms on the certain date', () => {
+    expect(hotel.getInformationByValue("2020/02/10", hotel.bookingRepo, 'bookingDate')).to.deep.equal([booking2])
+  });
+
+  it('should filter data by certain value', () => {
+    expect(hotel.getAvailableRooms(hotel.bookingRepo, hotel.roomRepo, "2020/01/24")).to.deep.equal([room1, room2, room4])
+  });
+
+  it('should get available Rooms on the certain date', () => {
+    expect(hotel.determineUpcomingBookings(booking1)).to.equal('Completed')
+  });
+
+  it('should get available Rooms on the certain date', () => {
+    expect(hotel.getTodayDate()).to.equal('2020/11/05')
+  });
+
+  it('should get available Rooms on the certain date', () => {
+    expect(hotel.filterBookingByDate(bookings)).to.deep.equal([booking2, booking1])
+  });
+});
