6598681807bd2dfa61381da538fbcd518d1f6b4f
Olga Morgan <olga.morgan17@gmail.com>
2020-11-06 18:05:59 -0700

    Add variables for api requests and function that creates hotel data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,59 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.scss';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import apiRequest from './apiRequests';
+// import domDisplay from './domDisplay';
+
+import Customer from './Customer';
+import CustomerRepo from './CustomerRepo';
+import Manager from './Manager';
+import Room from './Room';
+import RoomRepo from './RoomRepo';
+import Booking from './Booking';
+import BookingRepo from './BookingRepo';
+import Hotel from './Hotel';
+// import $ from 'jquery';
+
+let usersData;
+let bookingsData;
+let roomsData;
+
+let customerRepo;
+let bookingRepo;
+let roomRepo;
+let manager;
+let currentUser;
+let hotelOverlook;
+
+// const managerLoginInfo = {
+//   username: 'manager',
+//   password: 'overlook2020'
+// }
+
+// { username: customer50 (where 50 is the ID of the user)
+// password: overlook2020 }
+
+let receivedUsersData = apiRequest.getHotelData('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users', 'users');
+let receivedBookingsData = apiRequest.getHotelData('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', 'bookings');
+let receivedRoomsData = apiRequest.getHotelData('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms', 'rooms');
+
+Promise.all([receivedUsersData, receivedBookingsData, receivedRoomsData])
+  .then(value => {
+    usersData = value[0];
+    bookingsData = value[1];
+    roomsData = value[2];
+    createHotelData()
+  })
+  .catch(error => console.log(error))
+
+function createHotelData() {
+  const customers = usersData.map(user => new Customer(user.id, user.name));
+  const rooms = roomsData.map(room => new Room(room.number, room.roomType, room.bidet, room.bedSize, room.numBeds, room.costPerNight));
+  const bookings = bookingsData.map(booking => new Booking(booking.id, booking.userId, booking.date, booking.roomNumber, booking.roomServiceCharges));
+  customerRepo = new CustomerRepo(customers);
+  bookingRepo = new BookingRepo(bookings);
+  roomRepo = new RoomRepo(rooms);
+  hotelOverlook = new Hotel(bookingRepo, roomRepo, customerRepo);
+  // console.log(hotelOverlook)
+}
